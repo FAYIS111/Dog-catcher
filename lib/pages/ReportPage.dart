3d1eb74e4b9b93d73d3d5a8564cfda1862eea8bf
muhammed fayis mt <fayismt111@gmail.com>
2024-01-09 13:58:09 -0800

    Report page completed.And add to camara and picteres allows

--- lib/pages/ReportPage.dart
+++ lib/pages/ReportPage.dart
@@ -1,19 +1,218 @@
+import 'dart:io';
+import 'package:dog_catcher/widgets/AppButton.dart';
+import 'package:dog_catcher/widgets/Asset.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ReportPage extends StatefulWidget {
-  const ReportPage({super.key});
+  const ReportPage({Key? key}) : super(key: key);
 
   @override
   State<ReportPage> createState() => _ReportPageState();
 }
 
 class _ReportPageState extends State<ReportPage> {
+  File? imageFile;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.yellow,
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: Text(
+          'SELECT & CROP IMAGE',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20.0),
+            imageFile == null
+                ? Image.asset(
+                    Logo1,
+                    height: 300.0,
+                    width: 300.0,
+                  )
+                : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: double.infinity,
+                      height: 300,
+                      child: Card(
+                        shadowColor: Colors.grey,
+                        elevation: 14,
+                        child: Image.file(
+                          imageFile!,
+                          height: 300.0,
+                          width: 300.0,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+            const SizedBox(height: 20.0),
+            appButton(
+              color: MaterialStateProperty.all(Colors.greenAccent),
+              width: 150,
+              height: 50,
+              buttonText: "SELECT IMAGE",
+              buttonAction: () async {
+                Map<Permission, PermissionStatus> statuses = await [
+                  Permission.storage,
+                  Permission.camera,
+                ].request();
+                if (statuses[Permission.storage]!.isGranted &&
+                    statuses[Permission.camera]!.isGranted) {
+                  showImagePicker(context);
+                } else {
+                  print('no permission provided');
+                }
+              },
+            ),
+            appButton(
+              color: MaterialStateProperty.all(Colors.greenAccent),
+              width: 150,
+              height: 50,
+              buttonText: "REPORT",
+              buttonAction: () {},
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  final picker = ImagePicker();
+
+  void showImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (builder) {
+          return Card(
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 5.2,
+                margin: const EdgeInsets.only(top: 8.0),
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: InkWell(
+                      child: Column(
+                        children: const [
+                          Icon(
+                            Icons.image,
+                            size: 60.0,
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            "Gallery",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          )
+                        ],
+                      ),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.pop(context);
+                      },
+                    )),
+                    Expanded(
+                        child: InkWell(
+                      child: SizedBox(
+                        child: Column(
+                          children: const [
+                            Icon(
+                              Icons.camera_alt,
+                              size: 60.0,
+                            ),
+                            SizedBox(height: 12.0),
+                            Text(
+                              "Camera",
+                              textAlign: TextAlign.center,
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        _imgFromCamera();
+                        Navigator.pop(context);
+                      },
+                    ))
+                  ],
+                )),
+          );
+        });
+  }
+
+  _imgFromGallery() async {
+    await picker
+        .pickImage(source: ImageSource.gallery, imageQuality: 50)
+        .then((value) {
+      if (value != null) {
+        _cropImage(File(value.path));
+      }
+    });
+  }
+
+  _imgFromCamera() async {
+    await picker
+        .pickImage(source: ImageSource.camera, imageQuality: 50)
+        .then((value) {
+      if (value != null) {
+        _cropImage(File(value.path));
+      }
+    });
+  }
+
+  _cropImage(File imgFile) async {
+    final croppedFile = await ImageCropper().cropImage(
+        sourcePath: imgFile.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio16x9
+              ]
+            : [
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio5x3,
+                CropAspectRatioPreset.ratio5x4,
+                CropAspectRatioPreset.ratio7x5,
+                CropAspectRatioPreset.ratio16x9
+              ],
+        uiSettings: [
+          AndroidUiSettings(
+              toolbarTitle: "Image Cropper",
+              toolbarColor: Colors.deepOrange,
+              toolbarWidgetColor: Colors.white,
+              initAspectRatio: CropAspectRatioPreset.original,
+              lockAspectRatio: false),
+          IOSUiSettings(
+            title: "Image Cropper",
+          )
+        ]);
+    if (croppedFile != null) {
+      imageCache.clear();
+      setState(() {
+        imageFile = File(croppedFile.path);
+      });
+      // reload();
+    }
   }
 }
